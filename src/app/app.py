f3e3011b26ea256818ac3652e80af47684417ff6
hrkzz <hiro.suzuki.15@gmail.com>
2025-05-25 17:08:05 +0900

    change

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 import streamlit as st
 from openpyxl import load_workbook
 import tempfile
-import os
 from pathlib import Path
 from datetime import datetime
 
